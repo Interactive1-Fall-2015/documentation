0b28637f99e84b631c5b74ce5539d6be70e1dfbb
mikesawyer <msawyer@cca.edu>
2015-12-02 19:59:16 -0800

    kjbkj

---
+++ michael/excercises/excercise12/js/myscript.js
@@ -0,0 +1,25 @@
+$(function() {
+
+       $topIndex = 0;
+
+       $( ".ui-draggable" ).draggable().css({"position":"absolute","top":"0px" });;
+
+        $(".ui-draggable").click(function(){
+            $topIndex=$topIndex+1;
+            $(this).css("z-index",$topIndex);
+        });
+
+   });
+
+
+        $(function(){
+          $.okvideo({ source: 'https://www.youtube.com/watch?v=B5eT6TaEtPI',
+                    volume: 100})
+        });
+
+
+  $(function() {
+    $( "#resizable" ).resizable();
+  });
+
+ 
